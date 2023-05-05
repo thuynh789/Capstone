31977dd36de29804d5aea5b30215ea6e4e2e385e
tiana3113 <t.huynh3113@gmail.com>
2023-05-05 14:37:19 -0400

    scratchpad reducer

--- react-app/src/store/scratchpad.js
+++ react-app/src/store/scratchpad.js
@@ -12,3 +12,54 @@ export const updateScratchpad = (newScratchpad) => ({
 	payload: newScratchpad,
 });
 
+// THUNKS
+export const getScratchpadThunk = () => async (dispatch) => {
+	const res = await fetch("/api/scratchpad/");
+	if (res.ok) {
+		const data = await res.json();
+		dispatch(getScratchpad(data.scratchpad));
+		return null;
+	} else if (res.status < 500) {
+		const data = await res.json();
+		if (data.errors) {
+			return data.errors;
+		}
+	}
+	return { errors: ["An error occurred. Please try again."] };
+};
+
+export const updateScratchpadThunk = (scratchpad) => async (dispatch) => {
+	const res = await fetch(`/api/scratchpad/`, {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ content: scratchpad }),
+	});
+	if (res.ok) {
+		const data = await res.json();
+		dispatch(updateScratchpad(data));
+		return null;
+	} else if (res.status < 500) {
+		const data = await res.json();
+		if (data.errors) {
+			return data.errors;
+		}
+	}
+};
+
+// REDUCER
+export default function scratchpadReducer(state = {}, action) {
+	let newState = {};
+	switch (action.type) {
+		case GET_SCRATCHPAD:
+			newState[action.payload.id] = action.payload;
+			return newState;
+
+		case UPDATE_SCRATCHPAD:
+			newState[action.payload.id] = action.payload;
+			return newState;
+		default:
+			return state;
+	}
+}
